c3bf26d1cd11dbc2e100751049d03aa3ae62fa65
SarvarbekRozi <sarvarbekr1203@gmail.com>
2026-03-12 10:12:48 +0500

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Super admin',
+            'email' => 'admin@example.com',
+            'password' => 'admin2026',
         ]);
     }
 }
